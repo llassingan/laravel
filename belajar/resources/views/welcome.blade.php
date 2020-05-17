ea210f7cc422ba49e9adc15d6b6d0ecf3f9c1d90
Almanik B <almanik.balingga3@gmail.com>
2020-05-18 05:06:56 +0800

    initialization laravel

---
+++ belajar/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layout/defaultpage')
+
+@section('title', 'Belajar Mantap Jiwa')
+
+@section('container')
+
+<div class="container">
+<div class="row">
+<div class="col-10">
+<h1 class="mt-3">Hai, {{$n1}}</h1>
+</div>
+</div>
+</div>
+
+@endsection
---
+++ belajar/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great! 
+|
+*/
+
+// Route::get('/', function () {
+//     $n1="hoho";
+// 	return view('welcome',['n1' => $n1]);
+// });
+
+// Route::get('/about', function () {
+	
+// 	$n2="haha";
+//     return view('about',['n2' => $n2 ]);
+// });
+
+
+Route::get('/', 'mycont@index');
+Route::get('/about', 'mycont@about');
